82a1a14db7a83464ee8e7604ce5a5bba19618179
code-with-ShubhamS <ghostoperator846@gmail.com>
2024-10-03 15:19:44 +0530

    Headings

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaGithub } from 'react-icons/fa'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
 
 
 const Footer = () => {
@@ -10,8 +10,10 @@ const Footer = () => {
       <div className="w-full bg-gradient-to-r from-green-900 via-zinc-700 to-red-900 h-[1.5px] rounded-full  "></div>
       <div className='flex justify-between px-[30px] py-[15px]' >
         <p className='text-[#4b4b4b]'>2024 @Shubham Semwal</p>
+      
         <div >
-            <a href="https://github.com/code-with-ShubhamS" target='_blank' className='text-[#989898]'><FaGithub className="text-lg inline  mr-1 text-white"/>space</a>
+     
+            <a href="https://github.com/code-with-ShubhamS/PortFolio" target='_blank' rel="noopener noreferrer" className='text-[#989898] hover:cursor-pointer'> <FaGithub className="text-lg inline  mr-1 text-white"/>space</a>
         </div>
       </div>
       </div>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -10,7 +10,16 @@ const Project = () => {
 
 
         <div className=" flex justify-center items-center">
+      
           <div className="flex flex-col p-1 rounded-lg overflow-hidden max-w-[60rem] relative mt-[5rem] z-20">
+
+          <div className="text-center relative">
+        {/* Main Heading */}
+        <h1 className="text-[2.5rem] font-bold sm:text-6xl">Projects</h1>
+
+        {/* Subheading */}
+        <p className="text-lg mt-2 text-gray-400">Projects I've crafted.</p>
+      </div>
         
         {
             ProjectArr.map((ProjectArr,index)=>{
